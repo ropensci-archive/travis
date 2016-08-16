da2845d698cf6a4aaad4ead6bdbcb4ee2470a157
Kirill Müller <krlmlr+r@mailbox.org>
2016-08-16 23:05:40 +0200

    move

--- R/github.R
+++ R/github.R
@@ -13,19 +13,6 @@ github_info <- function(path = ".") {
   get_repo_data(repo)
 }
 
-github_url <- function(path = ".") {
-  r <- git2r::repository(path, discover = TRUE)
-  remote_names <- git2r::remotes(r)
-  if(!length(remote_names))
-    stop("Failed to lookup git remotes")
-  remote_name <- "origin"
-  if(!("origin" %in% remote_names)){
-    remote_name <- remote_names[1]
-    warning("No remote 'origin' found. Using: ", remote_name)
-  }
-  git2r::remote_url(r, remote_name)
-}
-
 #' @export
 #' @rdname github
 github_repo <- function(path = "."){
@@ -73,6 +60,19 @@ extract_repo <- function(path){
   }
   path <- sub("\\.git", "", path)
   sub("^https://github.com/", "", path)
+}
+
+github_url <- function(path = ".") {
+  r <- git2r::repository(path, discover = TRUE)
+  remote_names <- git2r::remotes(r)
+  if(!length(remote_names))
+    stop("Failed to lookup git remotes")
+  remote_name <- "origin"
+  if(!("origin" %in% remote_names)){
+    remote_name <- remote_names[1]
+    warning("No remote 'origin' found. Using: ", remote_name)
+  }
+  git2r::remote_url(r, remote_name)
 }
 
 auth_github <- function() {
