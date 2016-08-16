d8d2256c952f424fcfe4d555e72160d882f128fd
Kirill Müller <krlmlr+r@mailbox.org>
2016-08-16 23:02:47 +0200

    add extract_repo() tests

---
+++ tests/testthat/test-repo.R
@@ -0,0 +1,9 @@
+context("repo")
+
+test_that("extract_repo() works", {
+  repo <- "ropenscilabs/travis"
+
+  expect_equal(extract_repo("git://github.com/ropenscilabs/travis.git"), repo)
+  expect_equal(extract_repo("https://github.com/ropenscilabs/travis.git"), repo)
+  expect_equal(extract_repo("git@github.com:ropenscilabs/travis.git"), repo)
+})
