2f39a93770d83bf1738c456bed0a3430fa22e61d
Mika Braginsky <mikabr@stanford.edu>
2016-09-07 12:29:19 -0400

    first pass modularization

--- R/yaml.R
+++ R/yaml.R
@@ -1,10 +1,15 @@
-edit_travis_yml <- function(travis_yml) {
+edit_travis_yml <- function(travis_yml, tasks) {
   pkg <- "ropenscilabs/travis"
   if (!(pkg %in% travis_yml$r_github_packages)) {
     travis_yml$r_github_packages <- c(travis_yml$r_github_packages, pkg)
   }
-  script_command <- sprintf("R -e 'travis::deploy()'")
-  if (!(script_command %in% travis_yml$after_success)) {
+  base_fun <- "R -e 'travis::deploy"
+  task_arg <- sprintf("c(%s)", paste(tasks, collapse = ", "))
+  script_command <- sprintf("%s(tasks = %s)'", base_fun, task_arg)
+  in_after_success <- grepl(base_fun, travis_yml$after_success)
+  if (any(in_after_success)) {
+    travis_yml$after_success[which(in_after_success)] <- script_command
+  } else {
     travis_yml$after_success <- c(travis_yml$after_success, script_command)
   }
   return(travis_yml)
