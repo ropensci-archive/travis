3ca501f4d71fc065faa0703c373ce3777297e441
Mika Braginsky <mikabr@stanford.edu>
2016-09-07 16:25:15 -0400

    tasks in argument to deploy quoted properly

--- R/yaml.R
+++ R/yaml.R
@@ -4,7 +4,7 @@ edit_travis_yml <- function(travis_yml, tasks) {
     travis_yml$r_github_packages <- c(travis_yml$r_github_packages, pkg)
   }
   base_fun <- "R -e 'travis::deploy"
-  task_arg <- sprintf("c(%s)", paste(tasks, collapse = ", "))
+  task_arg <- sprintf('c("%s")', paste(tasks, collapse = ", "))
   script_command <- sprintf("%s(tasks = %s)'", base_fun, task_arg)
   in_after_success <- grepl(base_fun, travis_yml$after_success)
   if (any(in_after_success)) {
