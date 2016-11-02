43e0e856598b2d1801c25ecfc4ccfce830f6f423
Kirill Müller <krlmlr+r@mailbox.org>
2016-11-02 19:53:23 +0100

    deploy() parses beforehand

--- tests/testthat/test-deploy.R
+++ tests/testthat/test-deploy.R
@@ -1,9 +1,14 @@
 context("deploy")
 
+expect_parse <- function(x, code) {
+  expect_equal(unname(x), as.list(parse(text = code)))
+  expect_equal(names(x), code)
+}
+
 test_that("parse tasks", {
-  expect_equal(parse_task_env_value(" "), character())
-  expect_equal(parse_task_env_value("call1() call2()"),
+  expect_parse(parse_task_code(" "), character())
+  expect_parse(parse_task_code("call1( ); call2()"),
                c("call1()", "call2()"))
-  expect_equal(parse_task_env_value("call1() call2(1, 2, (3 + 4)+(5 + 6)) call3(4)"),
-               c("call1()", "call2(1, 2, (3 + 4)+(5 + 6))", "call3(4)"))
+  expect_parse(parse_task_code("call1(); call2(1, 2, (3 + 4) + (5 + 6)); call3(4)"),
+               c("call1()", "call2(1, 2, (3 + 4) + (5 + 6))", "call3(4)"))
 })
