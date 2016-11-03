f0a5fedd2b4050cd377af16ca20cc0537a3374ae
Kirill Müller <krlmlr+r@mailbox.org>
2016-11-03 23:52:39 +0100

    giving access to new user works

--- remake/setup.R
+++ remake/setup.R
@@ -1,26 +1,65 @@
 create_id <- function(value = NULL) {
   if (is.null(value)) {
     value <- gsub("_", "-", ids::adjective_animal())
+    message(value)
   }
   value
 }
 
-create_github_user <- function(user) {
+create_github_user <- function(user, password) {
   clipr::write_clip(user)
-  menu(c("OK"), title = paste0("Please create GitHub user ", user))
+  menu(c("OK"), title = paste0("Please create GitHub user ", user, " with password ", password))
+
+  user
 }
 
-user1 <- create_id("quasiprogressive-bustard")
-user2 <- create_id("plucky-bear")
+create_git_repo <- function(repo_path) {
+  unlink(repo_path, recursive = TRUE, force = TRUE)
+  dir.create(repo_path, recursive = TRUE, showWarnings = FALSE)
+  repo <- git2r::init(repo_path)
 
-create_github_user(user1)
-create_github_user(user2)
+  git2r::config(repo, user.name = "user.name", user.email = "user@ema.il")
 
-repo_base <- tempfile()
-dir.create(repo_base)
+  writeLines(character(), file.path(repo_path, ".gitignore"))
+  git2r::add(repo, ".gitignore")
+  git2r::commit(repo, "initial")
 
-repo1_path <- file.path(repo_base, "repo1")
-dir.create(repo1_path)
+  devtools::setup(repo_path)
+  git2r::add(repo, ".")
+  git2r::commit(repo, "infra")
 
-repo1 <- git2r::init(repo1_path)
-writeLines(character(), file.path(repo1_path, ".gitignore"))
+  repo
+}
+
+create_gh_repo <- function(user, password, repo) {
+  message("Current user: ", user)
+  clipr::write_clip(user)
+  r <- travis::github_create_repo(repo@path)
+  res <- httr::content(r)
+
+  remote_name <- "origin"
+  git2r::remote_add(repo, remote_name, gsub("//", paste0("//", user, ":", password, "@"), res$clone_url))
+
+  git2r::push(repo, remote_name, "refs/heads/master")
+
+  res$full_name
+}
+
+create_travis_token <- function(user) {
+  utils::browseURL("https://travis-ci.org")
+  menu("OK", title = paste0("Please authenticate user ", user, " for Travis"))
+  message("Please authenticate Travis user ", user)
+  travis::auth_travis()
+}
+
+enable_travis_for_repo <- function(repo, travis_token) {
+  withr::with_dir(
+    repo@path,
+    travis::travis_enable()
+  )
+}
+
+setup_keys_for_repo <- function(repo) {
+  1
+  travis::use_travis_deploy(repo@path)
+}
