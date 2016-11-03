e26c5e709f6c425b6ed27ed235f9186bd1bcd625
Kirill Müller <krlmlr+r@mailbox.org>
2016-11-03 23:49:41 +0100

    ask caller for desired scope

--- R/github.R
+++ R/github.R
@@ -108,13 +108,15 @@ extract_repo <- function(path) {
   sub("^https://github.com/", "", path)
 }
 
-auth_github_ <- function(cache = NULL) {
+auth_github_ <- function(cache = NULL, scopes = NULL) {
   message("Authenticating with GitHub")
   if (is.null(cache)) {
     cache <- getOption("httr_oauth_cache")
   }
 
-  scopes <- c("repo", "read:org", "user:email", "write:repo_hook")
+  if (is.null(scopes)) {
+    scopes <- c("repo", "read:org", "user:email", "write:repo_hook")
+  }
   app <- httr::oauth_app("github",
                          key = "4bca480fa14e7fb785a1",
                          secret = "70bb4da7bab3be6828808dd6ba37d19370b042d5")
