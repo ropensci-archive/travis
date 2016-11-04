bc5c9347658a79ceee74bd66887f9f5afd1d3ffc
Kirill Müller <krlmlr+r@mailbox.org>
2016-11-04 11:34:46 +0100

    get rid of sync arg

--- R/travis.R
+++ R/travis.R
@@ -198,18 +198,10 @@ travis_delete_var <- function(id, repo = github_repo(),
 
 #' @export
 #' @rdname travis
-travis_repo_info <- function(repo = github_repo(), sync = FALSE, token = travis_token(repo)) {
+travis_repo_info <- function(repo = github_repo(),
+                             token = travis_token(repo)) {
   req <- TRAVIS_GET(sprintf("/repos/%s", repo), token = token)
-  if (sync) {
-    if (identical(httr::content(req), list(file = "not found"))) {
-      message("Repo ", repo, " not found on Travis, synching", appendLF = FALSE)
-      travis_sync(token = token)
-      req <- TRAVIS_GET(sprintf("/repos/%s", repo), token = token)
-      httr::stop_for_status(req, sprintf("get repo info on %s from Travis after sync", repo))
-    }
-  } else {
-    httr::stop_for_status(req, sprintf("get repo info on %s from Travis. Try travis_repo_info(sync = TRUE)", repo))
-  }
+  httr::stop_for_status(req, sprintf("get repo info on %s from Travis", repo))
   httr::content(req)[[1L]]
 }
 
@@ -222,7 +214,7 @@ travis_repo_id <- function(repo = github_repo(), token = travis_token(repo), ...
 #' @export
 #' @rdname travis
 travis_enable <- function(active = TRUE, repo = github_repo(),
-                          token = travis_token(repo), repo_id = travis_repo_id(repo, sync = TRUE, token = token)) {
+                          token = travis_token(repo), repo_id = travis_repo_id(repo, token = token)) {
   req <- TRAVIS_PUT(sprintf("/hooks"), body = list(hook = list(id = repo_id, active = active)))
   httr::stop_for_status(
     req, sprintf(
@@ -258,7 +250,7 @@ setup_keys <- function(path, key_path, pub_key_path, enc_key_path) {
   # add tempkey and iv as secure environment variables on travis
   # TODO: overwrite if already exists
   travis_set_var("encryption_key", openssl::base64_encode(tempkey),
-                 public = FALSE, repo = repo, sync = TRUE)
+                 public = FALSE, repo = repo)
   travis_set_var("encryption_iv", openssl::base64_encode(iv),
                  public = FALSE, repo = repo)
 
