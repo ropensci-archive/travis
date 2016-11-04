c10cc197d0482767d867322e26b65b0c4bd7c15d
Kirill Müller <krlmlr+r@mailbox.org>
2016-11-04 17:21:24 +0100

    base64
    - The deploy key is encoded in the base64 encoding, because Travis doesn't handle environment variables with newlines very well.

--- R/setup.R
+++ R/setup.R
@@ -17,19 +17,25 @@ setup_keys <- function(path) {
 
   # generate deploy key pair
   key <- openssl::rsa_keygen()  # TOOD: num bits?
-  pub_key <- as.list(key)$pubkey
-
-  private_key <- character()
-  conn <- textConnection("private_key", "w")
-
-  openssl::write_pem(key, conn, password = NULL)
-  close(conn)
 
   # encrypt private key using tempkey and iv
   repo <- github_repo(path)
 
-  travis_set_var("id_rsa", paste(private_key, collapse = "\n"),
+  private_key <- encode_private_key(key)
+  travis_set_var("id_rsa", private_key,
                  public = FALSE, repo = repo)
 
+  pub_key <- as.list(key)$pubkey
   github_add_key(pub_key, path)
+}
+
+encode_private_key <- function(key) {
+  conn <- textConnection(NULL, "w")
+  openssl::write_pem(key, conn, password = NULL)
+  private_key <- textConnectionValue(conn)
+  close(conn)
+
+  private_key <- paste(private_key, collapse = "\n")
+
+  openssl::base64_encode(charToRaw(private_key))
 }
