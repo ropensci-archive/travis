4af44bc97eae3a511b5aa938a56545118f545f34
Kirill Müller <krlmlr+r@mailbox.org>
2017-06-09 23:17:56 +0200

    open browser window only in interactive mode, closes #52
    - Only interactive sessions will open a browser window to improve setup
      experience. The `travis_browse()` function still opens a browser
      window unconditionally (#52).

--- R/utils.R
+++ R/utils.R
@@ -1,17 +1,35 @@
 url_message <- function(..., url) {
   message(format_url_msg(..., url = url))
-  utils::browseURL(url)
+  open_browser_window(url)
 }
 
 url_stop <- function(..., url) {
-  utils::browseURL(url)
+  open_browser_window(url)
   stopc(format_url_msg(..., url = url))
 }
 
 format_url_msg <- function(..., url) {
-  paste0(..., ". Please visit\n  ", url, "\nA browser windows will be opened.")
+  paste0(..., ". Please visit\n  ", url, get_browser_window_text())
 }
 
 stopc <- function(...) {
   stop(..., call. = FALSE, domain = NA)
+}
+
+get_browser_window_text <- function() {
+  if (will_open_browser_window()) {
+    "\nA browser window will be opened."
+  } else {
+    ""
+  }
+}
+
+open_browser_window <- function(url) {
+  if (will_open_browser_window()) {
+    utils::browseURL(url)
+  }
+}
+
+will_open_browser_window <- function() {
+  interactive()
 }
