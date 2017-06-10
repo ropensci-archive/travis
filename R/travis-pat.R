8ad4fc1006724f4cfb98fa670c1f392b01f21842
Kirill Müller <krlmlr+r@mailbox.org>
2017-06-10 21:14:32 +0200

    document travis_set_pat()

--- R/travis-pat.R
+++ R/travis-pat.R
@@ -1,3 +1,18 @@
+#' Set the GITHUB_PAT variable on Travis
+#'
+#' Assigns a GitHub PAT to the `GITHUB_PAT` private variable on Travis CI
+#' via [travis_set_var()],
+#' among others this works around GitHub API rate limitations.
+#' By default, the PAT is obtained from [github_create_pat()].
+#'
+#' @param pat `[string]`\cr
+#'   If set, avoids calling `github_create_pat()`
+#' @inheritParams travis_repo_info
+#' @param repo_id `[string]`\cr
+#'   The repo ID, obtained from [travis_repo_id()] by default.
+#'
+#' @family Travis CI functions
+#'
 #' @export
 travis_set_pat <- function(pat = NULL,
                            repo = github_repo(),
