4c7ddbee6b0a6d920753f9ff117b7fe4bc710839
Kirill Müller <krlmlr+r@mailbox.org>
2017-06-10 20:54:45 +0200

    up package doc

--- R/travis-package.R
+++ R/travis-package.R
@@ -2,7 +2,14 @@
 #' Use [github_repo()] to get the name of the current repository
 #' as determined from the `origin` remote. The [github_info()]
 #' function queries basic information on your repository.
-#' The [travis_set_var()] and [github_add_key()] functions
-#' interact with the GitHub and Travis configuration of your repository,
-#' and ask for permission the first time you call them.
+#' The following functions simplify integrating R package testing and deployment
+#' with GitHub and Travis CI:
+#' - [github_create_repo()] creates a repository on GitHub,
+#' - [travis_enable()] enables Travis CI for your repository,
+#' - [travis_set_pat()] installs a `GITHUB_PAT` environment variable on Travis
+#'   to work around rate limitations for installing packages from GitHub,
+#' - [use_travis_deploy()] installs a public deploy key on GitHub and the
+#'   corresponding private key on Travis CI to simplify deployments to GitHub
+#'   from Travis CI.
+#' All these functions ask for permission the first time you call them.
 "_PACKAGE"
