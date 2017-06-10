3127d644ab76e40d43817bf6e7c2798e9032fad7
Kirill Müller <krlmlr+r@mailbox.org>
2017-06-10 10:46:36 +0200

    clipboard-based workflow for querying PAT

--- R/github.R
+++ R/github.R
@@ -174,3 +174,41 @@ check_write_org <- function(org, gh_token) {
     stopc("Must have role admin to edit organization ", org, ", not ", role_in_org)
   }
 }
+
+#' @export
+github_create_pat <- function(path = ".", repo = github_repo(path), pat = NULL) {
+  if (!is.null(pat)) {
+    return(pat)
+  }
+  if (!interactive()) {
+    stopc("`pat` must be set in non-interactive mode")
+  }
+
+  desc <- paste0("PAT for rtravis for ", repo)
+  clipr::write_clip(desc)
+  url_message(
+    "Create a personal access token, make sure that you are signed in as the correct user. ",
+    "The suggested description '", desc, "' has been copied to the clipboard. ",
+    "If you use this token only to avoid GitHub's rate limit, you can leave all scopes unchecked. ",
+    "Then, copy the new token to the clipboard, it will be detected and applied automatically.",
+    url = "https://github.com/settings/tokens/new"
+  )
+
+  wait_for_clipboard_pat()
+}
+
+wait_for_clipboard_pat <- function() {
+  message("Waiting for PAT to appear on the clipboard.")
+  repeat {
+    pat <- clipr::read_clip()
+    if (is_pat(pat)) break
+    Sys.sleep(0.1)
+  }
+  message("Detected PAT, clearing clipboard.")
+  clipr::write_clip("")
+  pat
+}
+
+is_pat <- function(pat) {
+  grepl("^[0-9a-f]{40}$", pat)
+}
--- R/travis.R
+++ R/travis.R
@@ -226,3 +226,12 @@ travis_delete_var <- function(id, repo = github_repo(),
 travis_browse <- function(repo = github_repo()) {
   utils::browseURL(paste0("https://travis-ci.org/", repo))
 }
+
+#' @export
+travis_set_pat <- function(pat = NULL,
+                           repo = github_repo(),
+                           token = travis_token(repo),
+                           repo_id = travis_repo_id(repo, token)) {
+  pat <- github_create_pat(repo = repo, pat = pat)
+  travis_set_var("GITHUB_PAT", pat, public = FALSE, token = token, repo_id = repo_id)
+}
