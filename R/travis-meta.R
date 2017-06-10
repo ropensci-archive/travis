7c118a05a6592877ddbed1435eb7938bb43472da
Kirill Müller <krlmlr+r@mailbox.org>
2017-06-10 21:01:56 +0200

    document meta functions

--- R/travis-meta.R
+++ R/travis-meta.R
@@ -1,3 +1,17 @@
+#' Retrieve meta information from Travis CI
+#'
+#' @description
+#' Return account, repositories, and user information.
+#'
+#' `travis_accounts()` queries the "/accounts" API.
+#'
+#' @param token `[Token2.0]`\cr
+#'   A Travis CI token obtained from [travis_token()] or [auth_travis()].
+#'
+#' @seealso [Travis CI API documentation](https://docs.travis-ci.com/api)
+#'
+#' @family Travis CI functions
+#'
 #' @export
 travis_accounts <- function(token = travis_token()) {
   req <- TRAVIS_GET("/accounts", query = list(all = 'true'), token = token)
@@ -5,14 +19,27 @@ travis_accounts <- function(token = travis_token()) {
   httr::content(req)[[1L]]
 }
 
+#' @description
+#' `travis_repositories()` queries the "/repos" API.
+#'
+#' @param slug,search `[string]`\cr
+#'   Arguments to the API call
+#'
 #' @export
+#'
+#' @rdname travis_accounts
 travis_repositories <- function(slug = NULL, search = NULL, token = travis_token()) {
   req <- TRAVIS_GET("/repos", query = list(slug = slug, search = search), token = token)
   httr::stop_for_status(req, paste("list repositories"))
   httr::content(req)[[1L]]
 }
 
+#' @description
+#' `travis_user()` queries the "/users" API.
+#'
 #' @export
+#'
+#' @rdname travis_accounts
 travis_user <- function(token = travis_token()) {
   req <- TRAVIS_GET("/users/", token = token)
   httr::stop_for_status(req, paste("get current user information"))
