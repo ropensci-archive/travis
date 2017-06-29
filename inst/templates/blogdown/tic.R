c613b581d731020955c8f46eb3aed31913fde96c
Kirill Müller <krlmlr+r@mailbox.org>
2017-06-29 09:00:17 +0200

    update templates
    - Update `tic.R` templates.

--- inst/templates/blogdown/tic.R
+++ inst/templates/blogdown/tic.R
@@ -2,10 +2,8 @@ get_stage("before_install") %>%
   add_step(step_run_code(update.packages(ask = FALSE)))
 
 get_stage("install") %>%
-  add_step(step_run_code(remotes::install_deps(dependencies = TRUE)))
-
-get_stage("deploy") %>%
-  add_step(step_run_code(blogdown::('index.Rmd', 'bookdown::gitbook')))
+  add_step(step_run_code(remotes::install_deps(dependencies = TRUE))) %>%
+  add_step(step_run_code(blogdown::install_hugo()))
 
 if (Sys.getenv("id_rsa") != "") {
   # pkgdown documentation can be built optionally. Other example criteria:
@@ -14,10 +12,9 @@ if (Sys.getenv("id_rsa") != "") {
   # - `Sys.getenv("BUILD_PKGDOWN") != ""`: If the env var "BUILD_PKGDOWN" is set
   # - `Sys.getenv("TRAVIS_EVENT_TYPE") == "cron"`: Only for Travis cron jobs
   get_stage("before_deploy") %>%
-    add_step(step_install_ssh_keys()) %>%
-    add_step(step_add_to_known_hosts("github.com")) %>%
-    add_step(step_test_ssh())
+    add_step(step_setup_ssh())
 
   get_stage("deploy") %>%
-    add_step(step_push_deploy(path = "blogdown_source/public", branch = "gh-pages"))
+    add_step(step_run_code({blogdown::build_site()})) %>%
+    add_step(step_push_deploy())
 }
