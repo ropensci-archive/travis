6b8cca76f3c739506e729024cd5391232a6609c9
Kirill Müller <krlmlr+r@mailbox.org>
2017-07-24 20:18:49 +0200

    add tests

---
+++ tests/testthat/test-brackets.R
@@ -0,0 +1,16 @@
+context("Brackets")
+
+test_that("Keeping and removing brackets", {
+  expect_equal(keep_brackets("updat[ing]{e} variable"), "updating variable")
+  expect_equal(remove_brackets("updat[ing]{e} variable"), "update variable")
+})
+
+test_that("Keeping and removing brackets (suffix only)", {
+  expect_equal(keep_brackets("restar[ting]{t} build"), "restarting build")
+  expect_equal(remove_brackets("restar[ting]{t} build"), "restart build")
+})
+
+test_that("Brackets with spaces are kept unchanged", {
+  expect_equal(keep_brackets("[a ]{b } c"), "[a ]{b } c")
+  expect_equal(remove_brackets("[a ]{b } c"), "[a ]{b } c")
+})
