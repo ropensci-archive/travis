5b3882aca911f32d9d8488c8fb52661be21c076a
Kirill Müller <krlmlr+r@mailbox.org>
2017-12-30 01:48:34 +0100

    Revert "build documentation in before_deploy step"
    "before_deploy" can't work because everything is stashed away at the beginning of the "deploy" stage. We need something in tic, maybe add_success_check()?
    
    https://github.com/ropenscilabs/tic/issues/32#issuecomment-354517888
    
    This reverts commit 4b372e19dcc6cc97494049ef507c0941b5270a98.

--- inst/templates/blogdown/tic.R
+++ inst/templates/blogdown/tic.R
@@ -11,9 +11,9 @@ if (Sys.getenv("id_rsa") != "") {
   # - `Sys.getenv("BUILD_PKGDOWN") != ""`: If the env var "BUILD_PKGDOWN" is set
   # - `Sys.getenv("TRAVIS_EVENT_TYPE") == "cron"`: Only for Travis cron jobs
   get_stage("before_deploy") %>%
-    add_step(step_setup_ssh()) %>%
-    add_code_step(blogdown::build_site())
+    add_step(step_setup_ssh())
 
   get_stage("deploy") %>%
+    add_code_step(blogdown::build_site()) %>%
     add_step(step_push_deploy())
 }
