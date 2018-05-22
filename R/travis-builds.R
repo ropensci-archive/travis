ca685881b5939e22878d82ad1231f056c335042f
Thomas J. Leeper <thosjleeper@gmail.com>
2018-05-22 09:42:21 +0100

    add travis_restart_last_build() function

--- R/travis-builds.R
+++ R/travis-builds.R
@@ -45,6 +45,26 @@ travis_restart_build <- function(build_id, repo = github_repo(), token = travis_
   invisible(httr::content(req)[[1]])
 }
 
+#' `travis_restart_last_build()` restarts the *last* build.
+#'
+#' @export
+#' @rdname travis_get_builds
+travis_restart_last_build <- function(repo = github_repo(), token = travis_token(repo),
+                                 repo_id = travis_repo_id(repo, token), quiet = FALSE) {
+  if (!is.numeric(repo_id)) stopc("`repo_id` must be a number")
+  last_build_id <- travis_get_builds(repo)$builds[[1]]$id
+  req <- TRAVIS_POST(paste0("/builds/", last_build_id, "/restart"), token = token)
+  check_status(
+    req,
+    sprintf(
+      "restar[ting]{t} build %s for %s (id: %s) from Travis CI",
+      build_id, repo, repo_id
+    ),
+    quiet
+  )
+  invisible(httr::content(req)[[1]])
+}
+
 #' `travis_cancel_build()` cancels a build with a given build ID.
 #'
 #' @export
