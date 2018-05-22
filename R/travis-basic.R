a4588e6d8ec79b19bc744ce5e6fe47c9ee0fc10b
Thomas J. Leeper <thosjleeper@gmail.com>
2018-05-22 09:33:36 +0100

    add s3 classes and print() methods

--- R/travis-basic.R
+++ R/travis-basic.R
@@ -19,7 +19,7 @@ travis_repo_info <- function(repo = github_repo(),
                              token = travis_token(repo)) {
   req <- TRAVIS_GET(sprintf("/repos/%s", repo), token = token)
   httr::stop_for_status(req, sprintf("get repo info on %s from Travis", repo))
-  httr::content(req)[[1L]]
+  structure(httr::content(req)[[1L]], class = "travis_repository")
 }
 
 #' @description
