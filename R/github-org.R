48ef003ddf8d1a345215dc3253929d4de057a304
pat-s <patrick.schratz@gmail.com>
2019-09-30 23:53:53 +0200

    apply to github org functions

--- R/github-org.R
+++ R/github-org.R
@@ -1,13 +1,12 @@
 github_user <- function() {
-  req = gh("GET /user")
+  req = gh::gh("GET /user")
   return(req)
 }
 
 get_role_in_repo <- function(repo) {
   user <- github_user()$login
 
-  browser()
-  req = gh(sprintf("GET /repos/%s/collaborators/%s/permission", repo, user))
+  req = gh::gh(sprintf("GET /repos/%s/collaborators/%s/permission", repo, user))
 
   message("get collaborator information on repo ", repo, " for user ", user)
 
@@ -21,21 +20,22 @@ check_admin_repo <- function(repo) {
   }
 }
 
-get_role_in_org <- function(org, gh_token) {
+get_role_in_org <- function(org) {
   browser()
-  gh(sprintf("GET /user/memberships/orgs/%s", org), .token = gh_token)
-  req <- GITHUB_GET(paste0("/user/memberships/orgs/", org), token = gh_token)
-  if (httr::status_code(req) %in% 403) {
-    org_perm_url <- paste0(
-      "https://github.com/orgs/", org,
-      "/policies/applications/551569"
-    )
+  req = gh::gh(sprintf("GET /user/memberships/orgs/%s", org))
 
-    url_stop(
-      "You may need to allow access for the rtravis GitHub app to your organization ", org,
-      url = org_perm_url
-    )
-  }
+  # FIXME: Query 403 exit code to check if rtravis is enabled for the repo
+  # if (httr::status_code(req) %in% 403) {
+  #   org_perm_url <- paste0(
+  #     "https://github.com/orgs/", org,
+  #     "/policies/applications/551569"
+  #   )
+  #
+  #   url_stop(
+  #     "You may need to allow access for the rtravis GitHub app to your organization ", org,
+  #     url = org_perm_url
+  #   )
+  # }
 
     message(sprintf(
       "query membership for organization ", org, ". ",
@@ -43,14 +43,11 @@ get_role_in_org <- function(org, gh_token) {
     )
     )
 
-  membership <- httr::content(req)
-  role_in_org <- membership$role
-
-  role_in_org
+ return(req$role)
 }
 
-check_write_org <- function(org, gh_token) {
-  role_in_org <- get_role_in_org(org, gh_token)
+check_write_org <- function(org) {
+  role_in_org <- get_role_in_org(org)
   if (role_in_org != "admin") {
     stopc("Must have role admin to edit organization ", org, ", not ", role_in_org)
   }
