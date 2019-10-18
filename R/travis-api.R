68f0eae55976a89486c311a15362d2437c939bfa
pat-s <patrick.schratz@gmail.com>
2019-10-18 18:37:53 +0200

    clean travis-api.R

--- R/travis-api.R
+++ R/travis-api.R
@@ -2,19 +2,6 @@ travis <- function(endpoint = "") {
   paste0("https://api.travis-ci.org", endpoint)
 }
 
-TRAVIS_GET_TEXT3 <- function(url, ..., token) {
-  TRAVIS_GET3(url, ..., accept = httr::accept("text/plain"), token = token)
-}
-
-TRAVIS_POST <- function(url, ..., encode = "json", token) {
-  httr::POST(travis(url),
-    httr::user_agent("ropenscilabs/travis"),
-    httr::accept("application/vnd.travis-ci.2+json"),
-    if (!is.null(token)) httr::add_headers(Authorization = paste("token", token)),
-    ...
-  )
-}
-
 TRAVIS_PATCH <- function(url, ..., token) {
   httr::PATCH(travis(url),
     encode = "json",
@@ -27,20 +14,6 @@ TRAVIS_PATCH <- function(url, ..., token) {
 
 TRAVIS_PATCH3 <- function(url, ..., token) {
   TRAVIS_PATCH(url, ..., httr::add_headers("Travis-API-Version" = 3), token = token)
-}
-
-TRAVIS_DELETE <- function(url, ..., token) {
-  httr::DELETE(travis(url),
-    encode = "json",
-    httr::user_agent("ropenscilabs/travis"),
-    httr::accept("application/vnd.travis-ci.2+json"),
-    httr::add_headers(Authorization = paste("token", token)),
-    ...
-  )
-}
-
-TRAVIS_DELETE3 <- function(url, ..., token) {
-  TRAVIS_DELETE(url, ..., httr::add_headers("Travis-API-Version" = 3), token = token)
 }
 
 encode_slug <- function(repo) {
