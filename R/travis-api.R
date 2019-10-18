142bf9b6d5adec7a6481ed285883c815e77d173f
pat-s <patrick.schratz@gmail.com>
2019-10-18 15:34:06 +0200

    remove TRAVISGET* functions

--- R/travis-api.R
+++ R/travis-api.R
@@ -2,23 +2,6 @@ travis <- function(endpoint = "") {
   paste0("https://api.travis-ci.org", endpoint)
 }
 
-TRAVIS_GET <- function(url, ..., accept = NULL, token) {
-  if (is.null(accept)) {
-    accept <- httr::accept("application/vnd.travis-ci.2+json")
-  }
-
-  httr::GET(travis(url),
-    httr::user_agent("ropenscilabs/travis"),
-    accept,
-    httr::add_headers(Authorization = paste("token", token)),
-    ...
-  )
-}
-
-TRAVIS_GET3 <- function(url, ..., token) {
-  TRAVIS_GET(url, ..., httr::add_headers("Travis-API-Version" = 3), token = token)
-}
-
 TRAVIS_GET_TEXT3 <- function(url, ..., token) {
   TRAVIS_GET3(url, ..., accept = httr::accept("text/plain"), token = token)
 }
