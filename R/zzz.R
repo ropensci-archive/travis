f97aaa7562e091a8099a0cb385a9eea0a52006a7
pat-s <patrick.schratz@gmail.com>
2019-11-14 10:21:57 +0100

    fix R_TRAVIS env var default

--- R/zzz.R
+++ R/zzz.R
@@ -4,6 +4,7 @@
   if (Sys.getenv("R_TRAVIS") != "") {
     cli::cli_text("{.pkg travis}: Using Travis endpoint '{Sys.getenv('R_TRAVIS')}' set via env var {.code R_TRAVIS}.")
   } else {
-    cli::cli_text("Env var 'R_TRAVIS' not set. Defaulting to '.org' endpoint.")
+    cli::cli_text("Env var 'R_TRAVIS' not set by user. Defaulting to '.org' endpoint.")
+    Sys.setenv("R_TRAVIS" = ".org")
   }
 }
