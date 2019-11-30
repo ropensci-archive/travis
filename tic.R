ed985869efd38040fc453dbc24cf449cef247719
pat-s <patrick.schratz@gmail.com>
2019-11-30 14:17:15 +0100

    pkgdown: dev and release sites

--- tic.R
+++ tic.R
@@ -3,5 +3,5 @@ do_package_checks()
 if (ci_has_env("BUILD_PKGDOWN")) {
   get_stage("install") %>%
     add_step(step_install_github("ropensci/rotemplate"))
-  do_pkgdown(orphan = TRUE)
+  do_pkgdown()
 }
