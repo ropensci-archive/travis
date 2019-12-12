2d4a183cabb30da611b2dc5b95f60e7b9b92484a
pat-s <patrick.schratz@gmail.com>
2019-12-12 08:54:48 +0100

    suggests vctrs for testthat

--- tic.R
+++ tic.R
@@ -1,8 +1,5 @@
 do_package_checks()
 
-get_stage("install") %>%
-  add_step(step_install_cran("vctrs"))
-
 if (ci_has_env("BUILD_PKGDOWN")) {
   get_stage("install") %>%
     add_step(step_install_github("ropensci/rotemplate"))
