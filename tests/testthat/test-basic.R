0c7d968d94ad5a787b886a812651e456b5949b78
pat-s <patrick.schratz@gmail.com>
2019-12-12 08:54:53 +0100

    style

--- tests/testthat/test-basic.R
+++ tests/testthat/test-basic.R
@@ -9,23 +9,26 @@ setwd("./travis-testthat")
 # })
 
 test_that("travis_has_repo()", {
-
   expect_true(travis_has_repo(repo = repo, endpoint = ".org"))
   expect_true(travis_has_repo(repo = repo, endpoint = ".com"))
 })
 
 test_that("travis_repo_id()", {
-
   expect_vector(travis_repo_id(repo = repo, endpoint = ".org"),
-                ptype = integer(), size = 1)
+    ptype = integer(), size = 1
+  )
   expect_vector(travis_repo_id(repo = repo, endpoint = ".com"),
-                ptype = integer(), size = 1)
+    ptype = integer(), size = 1
+  )
 })
 
 test_that("travis_repo_settings()", {
-
-  expect_is(travis_repo_settings(repo = repo, endpoint = ".org"),
-            "travis_settings")
-  expect_is(travis_repo_settings(repo = repo, endpoint = ".com"),
-            "travis_settings")
+  expect_is(
+    travis_repo_settings(repo = repo, endpoint = ".org"),
+    "travis_settings"
+  )
+  expect_is(
+    travis_repo_settings(repo = repo, endpoint = ".com"),
+    "travis_settings"
+  )
 })
