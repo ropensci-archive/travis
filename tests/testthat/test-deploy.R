87e35155a1d3f7fb9447a94160eb315d6334d9f7
pat-s <patrick.schratz@gmail.com>
2019-12-14 23:47:49 +0100

    skip deploy test

--- tests/testthat/test-deploy.R
+++ tests/testthat/test-deploy.R
@@ -13,6 +13,7 @@ test_that("use_travis_deploy detects existing keys (.org)", {
 
 test_that("use_travis_deploy detects existing keys (.com)", {
   # tests timing out on Travis for unknown reason
+  skip_on_ci()
   expect_message(
     use_travis_deploy(endpoint = ".com"),
     "Deploy key for Travis CI"
