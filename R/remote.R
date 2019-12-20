5d14a5a0018c85c78bd884c0e20cd0adf0f8365e
pat-s <patrick.schratz@gmail.com>
2019-12-20 16:25:21 +0100

    add more cases, merge into existing test file

--- R/remote.R
+++ R/remote.R
@@ -14,18 +14,33 @@ get_remote_url <- function(path) {
 
 extract_repo <- function(url) {
 
+  # account for http
+  if (grepl("http://github.com", url)) {
+    url <- sub("http://", "https://", url)
+  }
+
   # account for ssh notation
   if (grepl("^git@github.com:", url)) {
     url <- sub("^git@github.com:", "https://github.com/", url)
-  } else if (grepl("^git://github.com", url)) {
+  } else if (grepl("^git://github.com:", url)) {
     url <- sub("^git://github.com:", "https://github.com/", url)
+  } else if (grepl("^git://github.com", url)) {
+    url <- sub("^git://github.com", "https://github.com/", url)
   }
   # account for "www"
-    else if (grepl("^http://www.github.com", url)) {
+  if (grepl("http://www.github.com", url)) {
     url <- sub("http://www.github.com", "https://github.com", url)
-  } else if (grepl("^https://www.github.com", url)) {
+  } else if (grepl("https://www.github.com", url)) {
     url <- sub("https://www.github.com", "https://github.com", url)
   }
+
+  # account for "user/pass"
+  if (grepl("^http://(.+@)?github.com", url)) {
+    url <- sub("^http://(.+@)?github.com", "https://github.com", url)
+  } else if (grepl("^https://(.+@)github.com", url)) {
+    url <- sub("^https://(.+@)github.com", "https://github.com", url)
+  }
+
   if (!all(grepl("^https://github.com", url))) {
     stopc("Unrecognized repo format: ", url)
   }
--- tests/testthat/test-repo.R
+++ tests/testthat/test-repo.R
@@ -3,16 +3,16 @@ context("repo")
 test_that("extract_repo() works", {
   repo <- "ropenscilabs/travis"
 
-  expect_equal(extract_repo("git://github.com/ropenscilabs/travis.git"), repo)
-  expect_equal(extract_repo("http://github.com/ropenscilabs/travis.git"), repo)
-  expect_equal(extract_repo("http://user:pass@github.com/ropenscilabs/travis.git"), repo) # nolint
-  expect_equal(extract_repo("https://github.com/ropenscilabs/travis.git"), repo)
-  expect_equal(extract_repo("https://user:pass@github.com/ropenscilabs/travis.git"), repo) # nolint
-  expect_equal(extract_repo("git@github.com:ropenscilabs/travis.git"), repo)
-  expect_equal(extract_repo("git://github.com/ropenscilabs/travis"), repo)
-  expect_equal(extract_repo("http://github.com/ropenscilabs/travis"), repo)
-  expect_equal(extract_repo("http://user:pass@github.com/ropenscilabs/travis"), repo) # nolint
-  expect_equal(extract_repo("https://github.com/ropenscilabs/travis"), repo)
-  expect_equal(extract_repo("https://user:pass@github.com/ropenscilabs/travis"), repo) # nolint
-  expect_equal(extract_repo("git@github.com:ropenscilabs/travis"), repo)
+  expect_match(extract_repo("git://github.com/ropenscilabs/travis.git"), repo)
+  expect_match(extract_repo("http://github.com/ropenscilabs/travis.git"), repo)
+  expect_match(extract_repo("http://user:pass@github.com/ropenscilabs/travis.git"), repo) # nolint
+  expect_match(extract_repo("https://github.com/ropenscilabs/travis.git"), repo)
+  expect_match(extract_repo("https://user:pass@github.com/ropenscilabs/travis.git"), repo) # nolint
+  expect_match(extract_repo("git@github.com:ropenscilabs/travis.git"), repo)
+  expect_match(extract_repo("git://github.com/ropenscilabs/travis"), repo)
+  expect_match(extract_repo("http://github.com/ropenscilabs/travis"), repo)
+  expect_match(extract_repo("http://user:pass@github.com/ropenscilabs/travis"), repo) # nolint
+  expect_match(extract_repo("https://github.com/ropenscilabs/travis"), repo)
+  expect_match(extract_repo("https://user:pass@github.com/ropenscilabs/travis"), repo) # nolint
+  expect_match(extract_repo("git@github.com:ropenscilabs/travis"), repo)
 })
