7b32bc7b570d56afc840d5aadc51e18e0e4a0055
pat-s <patrick.schratz@gmail.com>
2019-12-25 18:44:13 +0100

    let pkgdown be built by jenkins

--- tic.R
+++ tic.R
@@ -1,7 +1,7 @@
+# installs dependencies, runs R CMD check, runs covr::codecov()
 do_package_checks()
 
-if (ci_has_env("BUILD_PKGDOWN")) {
-  get_stage("install") %>%
-    add_step(step_install_github("ropensci/rotemplate"))
+if (ci_on_travis()) {
+  # creates pkgdown site and pushes to gh-pages branch
   do_pkgdown()
 }
