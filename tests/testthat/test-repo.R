e4e81d6bfe5776a2ce36199846de2de7cdaaec81
pat-s <patrick.schratz@gmail.com>
2019-12-29 19:19:45 +0100

    expect_match -> expect_equal

--- tests/testthat/test-repo.R
+++ tests/testthat/test-repo.R
@@ -3,17 +3,17 @@ context("repo")
 test_that("extract_repo() works", {
   repo <- "ropenscilabs/travis"
 
-  expect_match(extract_repo("git://github.com/ropenscilabs/travis.git"), repo)
-  expect_match(extract_repo("http://github.com/ropenscilabs/travis.git"), repo)
-  expect_match(extract_repo("http://user:pass@github.com/ropenscilabs/travis.git"), repo) # nolint
-  expect_match(extract_repo("https://github.com/ropenscilabs/travis.git"), repo)
-  expect_match(extract_repo("https://user:pass@github.com/ropenscilabs/travis.git"), repo) # nolint
-  expect_match(extract_repo("git@github.com:ropenscilabs/travis.git"), repo)
-  expect_match(extract_repo("git://github.com/ropenscilabs/travis"), repo)
-  expect_match(extract_repo("http://github.com/ropenscilabs/travis"), repo)
-  expect_match(extract_repo("http://user:pass@github.com/ropenscilabs/travis"), repo) # nolint
-  expect_match(extract_repo("https://github.com/ropenscilabs/travis"), repo)
-  expect_match(extract_repo("https://user:pass@github.com/ropenscilabs/travis"), repo) # nolint
-  expect_match(extract_repo("git@github.com:ropenscilabs/travis"), repo)
-  expect_match(extract_repo("ssh://git@github.com/ropenscilabs/travis"), repo)
+  expect_equal(extract_repo("git://github.com/ropenscilabs/travis.git"), repo)
+  expect_equal(extract_repo("http://github.com/ropenscilabs/travis.git"), repo)
+  expect_equal(extract_repo("http://user:pass@github.com/ropenscilabs/travis.git"), repo) # nolint
+  expect_equal(extract_repo("https://github.com/ropenscilabs/travis.git"), repo)
+  expect_equal(extract_repo("https://user:pass@github.com/ropenscilabs/travis.git"), repo) # nolint
+  expect_equal(extract_repo("git@github.com:ropenscilabs/travis.git"), repo)
+  expect_equal(extract_repo("git://github.com/ropenscilabs/travis"), repo)
+  expect_equal(extract_repo("http://github.com/ropenscilabs/travis"), repo)
+  expect_equal(extract_repo("http://user:pass@github.com/ropenscilabs/travis"), repo) # nolint
+  expect_equal(extract_repo("https://github.com/ropenscilabs/travis"), repo)
+  expect_equal(extract_repo("https://user:pass@github.com/ropenscilabs/travis"), repo) # nolint
+  expect_equal(extract_repo("git@github.com:ropenscilabs/travis"), repo)
+  expect_equal(extract_repo("ssh://git@github.com/ropenscilabs/travis"), repo)
 })
