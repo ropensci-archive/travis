16294246f6ef860714f8c8dc5e7aad0ae03c39b5
pat-s <patrick.schratz@gmail.com>
2019-12-29 19:47:53 +0100

    debug travis

--- tests/testthat/helper.R
+++ tests/testthat/helper.R
@@ -2,6 +2,8 @@ if (!dir.exists("travis-testthat")) {
   system("git clone https://github.com/pat-s/travis-testthat.git")
 }
 
+print(here::here("tests/testthat/travis-testthat"))
+
 withr::with_dir(
   here::here("tests/testthat/travis-testthat"),
   {
