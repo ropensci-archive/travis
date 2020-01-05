d1194b72c0741b542e8b11829154daa319a5d62f
pat-s <patrick.schratz@gmail.com>
2020-01-05 18:44:28 +0100

    add tests for use_travis_deploy()

---
+++ tests/testthat/test-use-travis-deploy.R
@@ -0,0 +1,172 @@
+context("use_travis_deploy")
+
+withr::with_dir(
+  "travis-testthat",
+  {
+    test_that("use_travis_deploy works if the public key is missing", {
+      private_key_exists <- travis_get_vars(
+        # repo = repo,
+        endpoint = ".org"
+      ) %>%
+        purrr::map_lgl(~ .x$name == "Deploy key for Travis CI (.org)") %>%
+        any()
+
+      # delete existing public key from github
+      gh_keys <- gh::gh("/repos/:owner/:repo/keys",
+        owner = "pat-s", repo = "travis-testthat"
+      )
+      gh_keys_names <- gh_keys %>%
+        purrr::map_chr(~ .x$title)
+
+      public_key_exists <- any(gh_keys_names %in%
+        "Deploy key for Travis CI (.org)")
+
+      # delete public key
+      if (public_key_exists) {
+        key_id <- which(gh_keys_names %>%
+          purrr::map_lgl(~ .x == "Deploy key for Travis CI (.org)"))
+        gh::gh("DELETE /repos/:owner/:repo/keys/:key_id",
+          owner = "pat-s",
+          repo = "travis-testthat",
+          key_id = gh_keys[[key_id]]$id
+        )
+      }
+
+      # run function
+      use_travis_deploy()
+
+      # check again if both are present
+      private_key_exists <- travis_get_vars(
+        endpoint = ".org"
+      ) %>%
+        purrr::map_lgl(~ .x$name == "Deploy key for Travis CI (.org)") %>%
+        any()
+
+      # delete existing public key from github
+      gh_keys <- gh::gh("/repos/:owner/:repo/keys",
+        owner = "pat-s", repo = "travis-testthat"
+      )
+      gh_keys_names <- gh_keys %>%
+        purrr::map_chr(~ .x$title)
+
+      public_key_exists <- any(gh_keys_names %in%
+        "Deploy key for Travis CI (.org)")
+
+      expect_true(public_key_exists & private_key_exists)
+    })
+
+    test_that("use_travis_deploy works if the private key is missing", {
+      private_key_exists <- travis_get_vars(
+        # repo = repo,
+        endpoint = ".org"
+      ) %>%
+        purrr::map_lgl(~ .x$name == "Deploy key for Travis CI (.org)") %>%
+        any()
+
+      # delete existing public key from github
+      gh_keys <- gh::gh("/repos/:owner/:repo/keys",
+        owner = "pat-s", repo = "travis-testthat"
+      )
+      gh_keys_names <- gh_keys %>%
+        purrr::map_chr(~ .x$title)
+
+      public_key_exists <- any(gh_keys_names %in%
+        "Deploy key for Travis CI (.org)")
+
+      # delete private key
+      if (private_key_exists) {
+        # delete existing private key from Travis
+        travis_delete_var(travis_get_var_id("Deploy key for Travis CI (.org)",
+          quiet = TRUE
+        ),
+        endpoint = ".org"
+        )
+      }
+
+      # run function
+      use_travis_deploy()
+
+      # check again if both are present
+      private_key_exists <- travis_get_vars(
+        # repo = repo,
+        endpoint = ".org"
+      ) %>%
+        purrr::map_lgl(~ .x$name == "Deploy key for Travis CI (.org)") %>%
+        any()
+
+      # delete existing public key from github
+      gh_keys <- gh::gh("/repos/:owner/:repo/keys",
+        owner = "pat-s", repo = "travis-testthat"
+      )
+      gh_keys_names <- gh_keys %>%
+        purrr::map_chr(~ .x$title)
+
+      public_key_exists <- any(gh_keys_names %in%
+        "Deploy key for Travis CI (.org)")
+
+      expect_true(public_key_exists & private_key_exists)
+    })
+
+    test_that("use_travis_deploy works if the private key is missing", {
+      private_key_exists <- travis_get_vars(
+        # repo = repo,
+        endpoint = ".org"
+      ) %>%
+        purrr::map_lgl(~ .x$name == "Deploy key for Travis CI (.org)") %>%
+        any()
+
+      # delete existing public key from github
+      gh_keys <- gh::gh("/repos/:owner/:repo/keys",
+        owner = "pat-s", repo = "travis-testthat"
+      )
+      gh_keys_names <- gh_keys %>%
+        purrr::map_chr(~ .x$title)
+
+      public_key_exists <- any(gh_keys_names %in%
+        "Deploy key for Travis CI (.org)")
+
+      # delete private key
+      if (private_key_exists) {
+        # delete existing private key from Travis
+        travis_delete_var(travis_get_var_id("Deploy key for Travis CI (.org)",
+          quiet = TRUE
+        ),
+        endpoint = ".org"
+        )
+      }
+      # delete public key
+      if (public_key_exists) {
+        key_id <- which(gh_keys_names %>%
+          purrr::map_lgl(~ .x == "Deploy key for Travis CI (.org)"))
+        gh::gh("DELETE /repos/:owner/:repo/keys/:key_id",
+          owner = "pat-s",
+          repo = "travis-testthat",
+          key_id = gh_keys[[key_id]]$id
+        )
+      }
+
+      # run function
+      use_travis_deploy()
+
+      # check again if both are present
+      private_key_exists <- travis_get_vars(
+        # repo = repo,
+        endpoint = ".org"
+      ) %>%
+        purrr::map_lgl(~ .x$name == "Deploy key for Travis CI (.org)") %>%
+        any()
+
+      # delete existing public key from github
+      gh_keys <- gh::gh("/repos/:owner/:repo/keys",
+        owner = "pat-s", repo = "travis-testthat"
+      )
+      gh_keys_names <- gh_keys %>%
+        purrr::map_chr(~ .x$title)
+
+      public_key_exists <- any(gh_keys_names %in%
+        "Deploy key for Travis CI (.org)")
+
+      expect_true(public_key_exists & private_key_exists)
+    })
+  }
+)
