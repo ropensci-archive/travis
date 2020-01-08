f76ba921993e4cdf9375e4f0d9ba86657f81e92c
pat-s <patrick.schratz@gmail.com>
2020-01-08 15:42:09 +0100

    fiy travis-sync

--- tests/testthat/test-util.R
+++ tests/testthat/test-util.R
@@ -1,8 +1,16 @@
 context("sync")
 
-test_that("syncing works", {
-  expect_s3_class(
-    travis_sync(quiet = TRUE, endpoint = ".org"),
-    "travis_api"
-  )
-})
+withr::with_dir(
+  "travis-testthat",
+  {
+    test_that("syncing works", {
+
+      # syncing .org does not work if the Travis CI GH app is not installed
+      # unfortunately, this app cannot be installed anymore
+      expect_s3_class(
+        travis_sync(quiet = TRUE, endpoint = ".com"),
+        "travis_api"
+      )
+    })
+  }
+)
